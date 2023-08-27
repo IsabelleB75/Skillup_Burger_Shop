1cbd2cd865d7a8d5ccace9cf08a333c1e270d08e
= <ousaboua@yahoo.fr>
2023-08-27 20:41:03 +0200

    HashRouter change

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // App.
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/home/Home';
 import Footer from './components/layout/Footer';
 import Header from './components/layout/Header';
